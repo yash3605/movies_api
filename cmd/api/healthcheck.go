469e79c16a567063044dbfef3119c1a809fea775
yashpratap <pratapyashkulshrestha@gmail.com>
2025-07-09 22:04:25 +0530

    Response: Customized the sent JSON responses to increase readability

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,15 +5,17 @@ import (
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 
-	data := map[string]string{
-		"status":"available",
-		"environment": app.config.env,
-		"version": version,
+	env :=	envelope{
+		"status": "Available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version": version,
+		},
 	}
 
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+
+	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
-		app.logger.Error(err.Error())
-		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
 }
