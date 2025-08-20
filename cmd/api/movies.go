03d4e04c106764c17f207297b52f1096d74f2e91
yashprt <pratapyashkulshrestha@gmail.com>
2025-08-20 17:30:26 +0530

    Movies: Added a Dynamic Search of Movies by Title and Genres

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -172,9 +172,7 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 	var input struct {
 		Title	 string
 		Genres	 []string
-		Page	 int
-		PageSize int
-		Sort	 string
+		data.Filters
 	}
 
 	v := validator.New()
@@ -184,15 +182,26 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 	input.Title = app.readString(qs, "title", "")
 	input.Genres = app.readCSV(qs, "genres", []string{})
 
-	input.Page = app.readInt(qs, "page", 1, v)
-	input.PageSize = app.readInt(qs, "page_size", 20, v)
+	input.Filters.Page = app.readInt(qs, "page", 1, v)
+	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
 
-	input.Sort = app.readString(qs, "sort", "id")
+	input.Filters.Sort = app.readString(qs, "sort", "id")
 
-	if !v.Valid(){
+	input.Filters.SortSafeList = []string{"id", "title", "year", "runtime", "-id", "-title", "-year", "-runtime"}
+
+	if data.ValidateFilters(v, input.Filters); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
-	fmt.Fprintf(w, "%+v\n", input)
+	movies, err := app.models.Movies.GetAll(input.Title, input.Genres, input.Filters)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movies": movies}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
  }
---
+++ internal/data/filters.go
@@ -0,0 +1,21 @@
+package data
+
+import (
+	"github.com/greenlight/internal/validator"
+)
+
+type Filters struct {
+	Page			int
+	PageSize		int
+	Sort			string
+	SortSafeList	[]string
+}
+
+func ValidateFilters(v *validator.Validator, f Filters){
+	v.Check(f.Page > 0, "page", "Must be Greater than zero")
+	v.Check(f.Page <= 10_000_000, "page", "Must be a Maximum of 10 million")
+	v.Check(f.PageSize > 0, "page_size", "Must be greater than zero")
+	v.Check(f.PageSize <= 100, "page_size", "Must be a maximum of 100")
+
+	v.Check(validator.PermittedValue(f.Sort, f.SortSafeList...), "sort", "Invalid Sort Value")
+}
