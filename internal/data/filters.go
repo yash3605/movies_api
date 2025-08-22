a14af03dbdb4db522cec4512b9899434374c017f
yashprt <pratapyashkulshrestha@gmail.com>
2025-08-22 17:25:07 +0530

    Movies: Added Pagination (Pages and Page_size)

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -12,6 +12,14 @@ type Filters struct {
 	SortSafeList	[]string
 }
 
+type Metadata struct {
+	CurrentPage		int `json:"current_page,omitempty"`
+	PageSize		int `json:"page_size,omitempty"`
+	FirstPage		int `json:"first_page,omitempty"`
+	LastPage		int	`json:"last_page,omitempty"`
+	TotalRecords	int `json:"total_records,omitempty"`
+}
+
 func ValidateFilters(v *validator.Validator, f Filters){
 	v.Check(f.Page > 0, "page", "Must be Greater than zero")
 	v.Check(f.Page <= 10_000_000, "page", "Must be a Maximum of 10 million")
@@ -37,4 +45,26 @@ func (f Filters) sortDirection() string {
 	}
 
 	return "ASC"
+}
+
+func (f Filters) limit() int {
+	return f.PageSize
+}
+
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
+}
+
+func calculateMetadata(totalRecords, page, pageSize int) Metadata {
+	if totalRecords == 0 {
+		return Metadata{}
+	}
+
+	return Metadata{
+		CurrentPage: page,
+		PageSize: pageSize,
+		FirstPage: 1,
+		LastPage: (totalRecords + pageSize - 1) / pageSize,
+		TotalRecords: totalRecords,
+	}
 }
