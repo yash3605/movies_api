30c700bff9d28bc3eb928cb3ccc289d47701193c
yashprt <pratapyashkulshrestha@gmail.com>
2025-08-25 17:38:50 +0530

    App: Added a Rate Limiter for every Client

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -25,6 +25,12 @@ type config struct {
 		maxIdleConns int
 		maxIdleTime time.Duration
 	}
+
+	limiter struct {
+		rps		float64
+		burst	int
+		enabled	bool
+	}
 }
 
 type application struct {
@@ -43,6 +49,10 @@ func main() {
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "Postgres Max Open Connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "Postgres Max Idle Connections")
 	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "Postgres max Connections Idle time")
+
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limited")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
