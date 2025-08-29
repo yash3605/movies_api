dd6b70b60e329fffe0a18c0592b195a1ca85f11d
yashprt <pratapyashkulshrestha@gmail.com>
2025-08-29 17:36:42 +0530

    Server: Added a Graceful Shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"context"
 	"database/sql"
-	"net/http"
-	"fmt"
 	"flag"
 	"log/slog"
 	"os"
@@ -72,21 +70,11 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-
-	srv := &http.Server{
-		Addr: fmt.Sprintf(":%d", cfg.port),
-		Handler: app.routes(),
-		IdleTimeout: time.Minute,
-		ReadTimeout: 5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	err = app.serve()
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
 	}
-
-	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
-
-	err = srv.ListenAndServe()
-	logger.Error(err.Error())
-	os.Exit(1)
 }
 
 func openDB(cfg config) (*sql.DB, error){
